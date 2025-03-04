168fc2a62d6fc1a66bed089f53f25c45b74389aa
d-catte <d-catte@onu.edu>
2025-03-03 23:37:36 -0500

    Part A completed

--- src/main.rs
+++ src/main.rs
@@ -11,123 +11,184 @@ use std::{fs, thread};
 use text_io::read;
 
 fn main() {
-    let threads: u8;
-    print!("Number of threads: ");
-    threads = read!();
+    print!("Select Part (A/B)");
+    let part: String = read!();
 
-    let n_counter = Arc::new(AtomicU128::new(2));
+    if part.to_uppercase() == "A" {
+        loop {
+            let mut values: Vec<u32> = vec![0; 4];
+            println!("4 Random Values");
+            fill_array_rand(&mut values);
+            println!("Iteration 1: {:?}", values);
+            let _ = iterate(&mut values, true);
+            print!("\n");
 
-    let file = Arc::new(Mutex::new(
-        OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(get_filename())
-            .unwrap(),
-    ));
+            values.clear();
+            println!("4 Consecutive Tribonacci Values");
+            fill_array_trib(&mut values);
+            println!("Iteration 1: {:?}", values);
+            let _ = iterate(&mut values, true);
 
-    for _ in 0..threads {
-        let n_counter_clone = Arc::clone(&n_counter);
-        let file_clone = Arc::clone(&file);
-        thread::spawn(move || {
-            loop {
-                let n_size = n_counter_clone.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-                let count = 2_u128.pow(n_size as u32);
-                let mut values: Vec<i32> = vec![0; n_size as usize];
-                let mut result = String::new();
-                let mut largest: (String, i32) = (String::new(), 0);
+            println!("Run Again? (Y/N)");
+            let again: String = read!();
 
-                for i in 0..count {
-                    fill_array_inc(&mut values, i, n_size);
-                    let output = iterate(&mut values);
-                    result += &*output.0;
+            if again.to_uppercase() != "Y" {
+                break;
+            }
+        }
 
-                    if largest.1 < output.1 {
-                        largest = output;
+    } else {
+        let threads: u8;
+        print!("Number of threads: ");
+        threads = read!();
+
+        let n_counter = Arc::new(AtomicU128::new(2));
+
+        let file = Arc::new(Mutex::new(
+            OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(get_filename())
+                .unwrap(),
+        ));
+
+        for thread in 0..threads {
+            let n_counter_clone = Arc::clone(&n_counter);
+            let file_clone = Arc::clone(&file);
+            thread::spawn(move || {
+                println!("Thread: {}", thread);
+                loop {
+                    let n_size = n_counter_clone.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                    let count = 2_u128.pow(n_size as u32);
+                    let mut values: Vec<u32> = vec![0; n_size as usize];
+                    let mut result = String::new();
+                    let mut largest: (String, i32) = (String::new(), 0);
+
+                    for i in 0..count {
+                        fill_array_inc(&mut values, i, n_size);
+                        let output = iterate(&mut values, false);
+                        result += &*output.0;
+
+                        if largest.1 < output.1 {
+                            largest = output;
+                        }
                     }
+
+                    let final_output: String = "Length ".to_owned() + &*n_size.to_string() + &*" Largest: ".to_owned() + &*largest.0 + "\n";
+                    file_clone.lock().unwrap().write_all(final_output.as_bytes()).unwrap();
+                    println!("{} Has Completed", n_size);
                 }
+            });
+        }
 
-                let final_output: String = "Largest: ".to_owned() + &*largest.0 + "\n" + &*result + "\n";
-                file_clone.lock().unwrap().write_all(final_output.as_bytes()).unwrap();
-                println!("{} Has Completed", n_size);
+        loop {
+        }
+    }
+}
+
+fn get_filename() -> String {
+    let date = Local::now().format("%Y-%m-%d").to_string();
+    let mut filename = format!("output_{}.txt", date);
+    let mut ending: i32 = 0;
+    while file_exists(&filename) {
+        filename.remove_matches(format!(" ({})", ending).as_str());
+        filename.remove_matches(".txt");
+        ending += 1;
+        filename += format!(" ({}).txt", ending).as_str();
+    }
+    filename
+}
+
+/// Fills the array with randomized values
+fn fill_array_rand(values: &mut Vec<u32>) {
+    for i in 0..values.len() {
+        values[i] = rand::rng().random_range(0..10000);
+    }
+}
+
+/// Fill with random consecutive Tribannoci values
+fn fill_array_trib(values: &mut Vec<u32>) {
+    let random_index = rand::rng().random_range(0..25);
+    values.append(&mut generate_trib_sequence(random_index));
+}
+
+/// Gets the 4th tribanocci values after the start index (including the start index)
+fn generate_trib_sequence(start_index: i32) -> Vec<u32> {
+    let mut previous_vals: [u32; 3] = [0, 0, 1];
+    let mut return_vals: Vec<u32> = vec![0, 0, 1];
+    for i in 3..=start_index + 3 {
+        let next_val = previous_vals[0] + previous_vals[1] + previous_vals[2];
+        previous_vals[0] = previous_vals[1];
+        previous_vals[1] = previous_vals[2];
+        previous_vals[2] = next_val;
+        if i >= start_index {
+            if return_vals.len() >= 4 {
+                return_vals.remove(0);
             }
-        });
-    }
-
-    fn get_filename() -> String {
-        let date = Local::now().format("%Y-%m-%d").to_string();
-        let mut filename = format!("output_{}.txt", date);
-        let mut ending: i32 = 0;
-        while file_exists(&filename) {
-            filename.remove_matches(format!(" ({})", ending).as_str());
-            filename.remove_matches(".txt");
-            ending += 1;
-            filename += format!(" ({}).txt", ending).as_str();
-        }
-        filename
-    }
-
-    /// Fills the array with randomized values
-    fn fill_array(values: &mut Vec<i32>) {
-        for i in 0..values.len() {
-            values[i] = rand::rng().random_bool(0.5) as i32;
+            return_vals.push(next_val);
         }
     }
+    return_vals
+}
 
-    fn fill_array_inc(values: &mut Vec<i32>, count: u128, n_size: u128) {
-        values.clear();
-        let string = format!("{count:b}");
-        let sign_extend = n_size - string.len() as u128;
+/// Fill with binary value
+fn fill_array_inc(values: &mut Vec<u32>, count: u128, n_size: u128) {
+    values.clear();
+    let string = format!("{count:b}");
+    let sign_extend = n_size - string.len() as u128;
 
-        let sign_extended_string = if sign_extend > 0 {
-            "0".repeat(sign_extend.try_into().unwrap()) + &string
+    let sign_extended_string = if sign_extend > 0 {
+        "0".repeat(sign_extend.try_into().unwrap()) + &string
+    } else {
+        string
+    };
+
+    for char in sign_extended_string.chars() {
+        values.push(char as u32 - 48);
+    }
+}
+
+/// Iterates through the array.
+/// Subtracts until all values are 0
+fn iterate(values: &mut Vec<u32>, debug: bool) -> (String, i32) {
+    let mut saved: String = String::from(format!("{:?}", values) + " : ");
+    let mut iter: i32 = 1;
+    while !is_zero(values) {
+        iter += 1;
+        subtract(values);
+        if debug {
+            println!("Iteration {}: {:?}", iter, values);
+        }
+    }
+    saved += &iter.to_string();
+    saved += "\n";
+    (saved, iter)
+}
+
+/// Checks if all values are zero
+fn is_zero(values: &Vec<u32>) -> bool {
+    for i in 0..values.len() {
+        if values[i] != 0 {
+            return false;
+        }
+    }
+    true
+}
+
+/// Subtracts the next array value from the previous one.
+/// Wraps around once hitting the end
+fn subtract(values: &mut Vec<u32>) {
+    let original_val: u32 = values[0];
+    for i in 0..values.len() {
+        if i + 1 < values.len() {
+            values[i] = (values[i] as i64 - values[i + 1] as i64).abs() as u32;
         } else {
-            string
-        };
-
-        for char in sign_extended_string.chars() {
-            values.push(char as i32 - 48);
+            values[i] = (values[i] as i64 - original_val as i64).abs() as u32;
         }
     }
+}
 
-    /// Iterates through the array.
-    /// Subtracts until all values are 0
-    fn iterate(values: &mut Vec<i32>) -> (String, i32) {
-        let mut saved: String = String::from(format!("{:?}", values) + " : ");
-        let mut iter: i32 = 0;
-        while !is_zero(values) {
-            iter += 1;
-            subtract(values);
-        }
-        saved += &iter.to_string();
-        saved += "\n";
-        (saved, iter)
-    }
-
-    /// Checks if all values are zero
-    fn is_zero(values: &Vec<i32>) -> bool {
-        for i in 0..values.len() {
-            if values[i] != 0 {
-                return false;
-            }
-        }
-        true
-    }
-
-    /// Subtracts the next array value from the previous one.
-    /// Wraps around once hitting the end
-    fn subtract(values: &mut Vec<i32>) {
-        let original_val: i32 = values[0];
-        for i in 0..values.len() {
-            if i + 1 < values.len() {
-                values[i] = (values[i] - values[i + 1]).abs();
-            } else {
-                values[i] = (values[i] - original_val).abs();
-            }
-        }
-    }
-
-    /// Checks if a file with the filename exists
-    fn file_exists(filename: &str) -> bool {
-        fs::metadata(filename).is_ok()
-    }
+/// Checks if a file with the filename exists
+fn file_exists(filename: &str) -> bool {
+    fs::metadata(filename).is_ok()
 }
